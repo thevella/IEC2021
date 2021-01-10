088ac630f63db05c2ceac48a68024c161ce8331c
mitchell <thevella@users.noreply.github.com>
2021-01-10 12:33:53 -0500

    speed control

--- sketch_jan10a.cpp/sketch_jan10a.cpp.ino
+++ sketch_jan10a.cpp/sketch_jan10a.cpp.ino
@@ -1,45 +1,55 @@
-int motorLeftDirPin = 2;
-int motorLeftOnPin = 3;
+int motorLeftPin1 = 2;
+int motorLeftPin2 = 3;
 
-int motorRightDirPin = 4;
-int motorRightOnPin = 5;
+int motorRightPin1 = 4;
+int motorRightPin2 = 5;
 
-int motorSpeedPinLeft = 9;
-int motorSpeedPinRight = 10;
+int motorSpeedPin = 6;
 
+int 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motorLeftDirPin, OUTPUT);
-  pinMode(motorLeftOnPin, OUTPUT);
-  pinMode(motorRightDirPin, OUTPUT);
-  pinMode(motorRightOnPin, OUTPUT);
+  pinMode(motorLeftPin1, OUTPUT);
+  pinMode(motorLeftPin2, OUTPUT);
+  pinMode(motorRightPin1, OUTPUT);
+  pinMode(motorRightPin2, OUTPUT);
 
-  pinMode(motorSpeedPinLeft, OUTPUT);
-  pinMode(motorSpeedPinRight, OUTPUT);
+  pinMode(motorSpeedPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:   
 
   //Controlling speed (0 = off and 255 = max speed):
-  analogWrite(motorSpeedPinLeft, 250); //ENA pin
-  analogWrite(motorSpeedPinRight, 250);
-
+  analogWrite(motorSpeedPin, 100); //ENA pin
   //Controlling spin direction of motors:
-  digitalWrite(motorLeftDirPin, HIGH);
-  digitalWrite(motorLeftOnPin, LOW);
+  digitalWrite(motorLeftPin1, HIGH);
+  digitalWrite(motorLeftPin2, LOW);
 
-  digitalWrite(motorRightDirPin, HIGH);
-  digitalWrite(motorRightOnPin, LOW);
+  digitalWrite(motorRightPin1, HIGH);
+  digitalWrite(motorRightPin2, LOW);
   delay(1000);
 
-  digitalWrite(motorLeftDirPin, LOW);
-  digitalWrite(motorLeftOnPin, HIGH);
+  digitalWrite(motorLeftPin1, LOW);
+  digitalWrite(motorLeftPin2, HIGH);
 
-  digitalWrite(motorRightDirPin, LOW);
-  digitalWrite(motorRightOnPin, HIGH);
+  digitalWrite(motorRightPin1, LOW);
+  digitalWrite(motorRightPin2, HIGH);
   delay(1000);
+}
+
+void setDir(int pin1, int pin2, int dir){
+  if (dir == 0){
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, LOW);
+  } else if (dir == 1){
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, HIGH);
+  } else if (dir == 2) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, LOW);
+  }
 }
 
 void turn(int deg){
